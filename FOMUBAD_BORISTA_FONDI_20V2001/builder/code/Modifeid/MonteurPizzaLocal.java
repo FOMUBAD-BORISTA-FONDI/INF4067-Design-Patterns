4ce56cb3622501da0755329a15082ec0d63fa948
FOMUBAD-BORISTA-FONDI <borista.fomubad@facsciences-uy1.cm>
2023-10-17 08:02:02 +0100

    Added Builder

---
+++ FOMUBAD_BORISTA_FONDI_20V2001/builder/code/Modifeid/MonteurPizzaLocal.java
@@ -0,0 +1,20 @@
+package FOMUBAD_BORISTA_FONDI_20V2001.builder.code.Modifeid;
+
+public class MonteurPizzaLocal extends MonteurPizza{
+
+    @Override
+    public void monterPate() {
+        pizza.setPate("feuille de macabo");
+    }
+
+    @Override
+    public void monterSauce() {
+        pizza.setSauce("escargo");
+    }
+
+    @Override
+    public void monterGarniture() {
+        pizza.setGarniture("sauce jaune");
+    }
+    
+}
---
+++ FOMUBAD_BORISTA_FONDI_20V2001/builder/code/Original/Client.java
@@ -0,0 +1,15 @@
+package FOMUBAD_BORISTA_FONDI_20V2001.builder.code.Original;
+
+public class Client {
+    
+    public static void main(String[] args) {
+        MonteurPizza monteurPizzaPiquante = new MonteurPizzaPiquante();
+        MonteurPizza monteurPizzaReine = new MonteurPizzaReine();
+
+        Directeur directeur = new Directeur(monteurPizzaReine);
+
+        directeur.Construire();
+        directeur.setMonteurPizza(monteurPizzaPiquante);
+        directeur.Construire();
+    }
+}
