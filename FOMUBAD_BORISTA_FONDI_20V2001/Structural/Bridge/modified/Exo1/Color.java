ee6dfae631acb236293e3e07c4038dd4fd0e7e01
FOMUBAD-BORISTA-FONDI <borista.fomubad@facsciences-uy1.cm>
2023-12-02 02:35:19 +0100

    Deleted some files

--- FOMUBAD_BORISTA_FONDI_20V2001/Structural/Bridge/modified/Exo1/Color.java
+++
@@ -1,5 +0,0 @@
-package FOMUBAD_BORISTA_FONDI_20V2001.Structural.Bridge.modified.Exo1;
-
-public interface Color {
-    public void fillColor();
-}
--- FOMUBAD_BORISTA_FONDI_20V2001/Structural/Bridge/modified/Exo1/RedColor.java
+++
@@ -1,8 +0,0 @@
-package FOMUBAD_BORISTA_FONDI_20V2001.Structural.Bridge.modified.Exo1;
-
-public class RedColor implements Color{
-    @Override
-    public void fillColor() {
-        System.out.println("red Color");
-    }
-}
