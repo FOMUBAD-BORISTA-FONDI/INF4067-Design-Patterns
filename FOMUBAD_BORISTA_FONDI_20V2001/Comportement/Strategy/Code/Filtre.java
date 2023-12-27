cdeb7361eaaf0d3995cb945c9b61c68a202e6eed
FOMUBAD-BORISTA-FONDI <borista.fomubad@facsciences-uy1.cm>
2023-12-27 22:19:31 +0100

    Template and Strategy First push

---
+++ FOMUBAD_BORISTA_FONDI_20V2001/Comportement/Strategy/Code/Filtre.java
@@ -0,0 +1,7 @@
+
+public interface Filtre {
+
+    String getNom();
+
+    void appliquer(Image image);
+}
---
+++ FOMUBAD_BORISTA_FONDI_20V2001/Comportement/Strategy/Code/FiltreNoirBlanc.java
@@ -0,0 +1,13 @@
+
+public class FiltreNoirBlanc implements Filtre {
+
+    @Override
+    public String getNom() {
+        return "Noir et blanc";
+    }
+
+    @Override
+    public void appliquer(Image image) {
+       System.out.println("Implémenter le filtre noir blanc sur l'image");
+    }
+}
